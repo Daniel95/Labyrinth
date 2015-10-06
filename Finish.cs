a467d7e191cc5f4fdc87276eef724421faa66274
nathan-san <18964@ma-web.nl>
2015-10-06 12:05:07 +0200

    finish functie toegevoegd.

---
+++ Finish.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finish : MonoBehaviour {
+	[SerializeField]
+	private int _levelNumber;
+
+	// Use this for initialization
+	void OnCollisionEnter(Collision obj) {
+		if (obj.gameObject.tag == "Player") {
+			Application.LoadLevel(_levelNumber);
+		}
+	}
+}
