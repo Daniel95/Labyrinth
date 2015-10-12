b1301b6e2d0b502c3851b8340f12e3940c0f1d65
Daniel95 <idaniel95@me.com>
2015-10-12 18:56:43 +0200

    Final version of carryother & changed movewall to movetodest
    MoveToDest is now alot more efficient and flexible.

---
+++ Object Control/MoveToDest.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToDest : MonoBehaviour 
+{
+	[SerializeField]
+	private float speed;
+	[SerializeField]
+	private Transform endSpot;
+	[SerializeField]
+	private Transform originSpot;
+
+	private Transform _destination;
+	private bool _activated;
+
+	void Start(){
+		_destination = originSpot;
+	}
+
+	void Update()
+	{
+		if (_activated) {
+			var move = GoTo (transform.position);
+			transform.position = move;
+		}
+	}
+	
+	void OnEnable()
+	{
+		KeyboardInput.MoveObjects += SetActive;
+	}
+	
+	void OnDiable()
+	{
+		KeyboardInput.MoveObjects -= SetActive;
+	}
+
+	public Vector3 GoTo(Vector3 currentPos) {
+		var pos = Vector3.MoveTowards(currentPos, _destination.position, speed);
+		if(pos == _destination.position) _activated = false;
+		return pos;
+	}
+	
+	void SetActive()
+	{
+		_activated = true;
+		if(_destination == originSpot) _destination = endSpot;
+		else _destination = originSpot;
+	}
+
+	public bool checkActive {
+		get {return _activated; }
+	}
+}
